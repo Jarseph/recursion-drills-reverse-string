4d8594bbfece309ae56834bae41b30e7fed1f0f8
Joseph Spencer <104227087+Jarseph@users.noreply.github.com>
2022-08-19 17:33:09 -0700

    Add files via upload

---
+++ src/reverse.js
@@ -0,0 +1,12 @@
+/**
+ * Returns the string with the characters in reverse order
+ * @param {string} text the string to be reversed
+ */
+function reverse(text) {
+ if (text === "")
+    return "";
+  else
+    return reverse(text.substr(1)) + text.charAt(0);
+}
+
+module.exports = reverse;
